1de56bafcac14fde86ca437b11df550ef9a8af4a
Kangbo Lu <redstonegranola@gmail.com>
2016-12-09 11:32:29 -0800

    Add file via Github
    Working code for base and hinge leg

---
+++ baseLegTest.ino
@@ -0,0 +1,21 @@
+int servoPin = 7;
+bool flag = true;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(servoPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int pulseTime = 1250; // miliseconds to pause
+  
+  if (flag) {
+    digitalWrite(servoPin, HIGH);
+    delayMicroseconds(pulseTime);
+    digitalWrite(servoPin, LOW);
+  }
+
+  flag = false;
+  delay(25);
+}
